408fd258dec17c8143059d3d828b829e39ce959c
SorrowBlue <sorrowblue.dev@gmail.com>
2026-06-13 05:54:17 +0900

    refactor(build): extract test configuration to testing plugin

---
+++ build-logic/src/main/kotlin/kioarch.testing.gradle.kts
@@ -0,0 +1,106 @@
+import org.gradle.api.tasks.testing.Test
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest
+import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeTest
+
+plugins {
+    // No external plugins need to be applied here.
+}
+
+val generateTestFiles = tasks.register("generateTestFiles", JavaExec::class.java) {
+    group = "verification"
+    description = "Generates test archives for testing"
+    dependsOn("jvmTestClasses")
+    mainClass.set("com.sorrowblue.kioarch.TestFileGenerator")
+    args(layout.buildDirectory.dir("tmp/large_tests").get().asFile.absolutePath)
+}
+
+plugins.withId("org.jetbrains.kotlin.multiplatform") {
+    val kotlin = extensions.getByType(KotlinMultiplatformExtension::class.java)
+    generateTestFiles.configure {
+        classpath = kotlin.targets.getByName("jvm").compilations.getByName("test").let {
+            files(it.output.allOutputs, it.runtimeDependencyFiles)
+        }
+    }
+}
+
+val copyJsTestAssets = tasks.register("copyJsTestAssets", Copy::class.java) {
+    dependsOn(generateTestFiles)
+    from(layout.buildDirectory.dir("tmp/large_tests"))
+    into(project.rootProject.layout.buildDirectory.dir("js/packages/KioArch-root-kioarch-test/kotlin"))
+}
+
+val copyWasmTestAssets = tasks.register("copyWasmTestAssets", Copy::class.java) {
+    dependsOn(generateTestFiles)
+    from(layout.buildDirectory.dir("tmp/large_tests"))
+    into(project.rootProject.layout.buildDirectory.dir("wasm/packages/KioArch-root-kioarch-test/kotlin"))
+}
+
+tasks.withType(KotlinNativeTest::class.java).configureEach {
+    dependsOn(generateTestFiles)
+    val testDir = layout.buildDirectory.dir("tmp/large_tests").get().asFile.absolutePath
+    environment("LARGE_ZIP_PATH", "$testDir/large.zip")
+    environment("LARGE_7Z_PATH", "$testDir/large.7z")
+    environment("LARGE_TARGZ_PATH", "$testDir/large.tar.gz")
+    environment("LARGE_100M_7Z_PATH", "$testDir/large_100m.7z")
+    environment("LARGE_100M_TARGZ_PATH", "$testDir/large_100m.tar.gz")
+    environment("LARGE_100M_ZIP_PATH", "$testDir/large_100m.zip")
+    environment("TEST_BZ2_PATH", "$testDir/test.bz2")
+    environment("TEST_TARBZ2_PATH", "$testDir/test.tar.bz2")
+
+    // Pass environment variables to the iOS Simulator test runner
+    environment("SIMCTL_CHILD_LARGE_ZIP_PATH", "$testDir/large.zip")
+    environment("SIMCTL_CHILD_LARGE_7Z_PATH", "$testDir/large.7z")
+    environment("SIMCTL_CHILD_LARGE_TARGZ_PATH", "$testDir/large.tar.gz")
+    environment("SIMCTL_CHILD_LARGE_100M_7Z_PATH", "$testDir/large_100m.7z")
+    environment("SIMCTL_CHILD_LARGE_100M_TARGZ_PATH", "$testDir/large_100m.tar.gz")
+    environment("SIMCTL_CHILD_LARGE_100M_ZIP_PATH", "$testDir/large_100m.zip")
+    environment("SIMCTL_CHILD_TEST_BZ2_PATH", "$testDir/test.bz2")
+    environment("SIMCTL_CHILD_TEST_TARBZ2_PATH", "$testDir/test.tar.bz2")
+}
+
+tasks.withType(KotlinJsTest::class.java).configureEach {
+    dependsOn(generateTestFiles)
+    val testDir = layout.buildDirectory.dir("tmp/large_tests").get().asFile.absolutePath
+    environment("LARGE_ZIP_PATH", "$testDir/large.zip")
+    environment("LARGE_7Z_PATH", "$testDir/large.7z")
+    environment("LARGE_TARGZ_PATH", "$testDir/large.tar.gz")
+    environment("TEST_7Z_PATH", "$testDir/test.7z")
+    environment("TEST_ZIP_PATH", "$testDir/test.zip")
+    environment("TEST_SJIS_ZIP_PATH", "$testDir/test_sjis.zip")
+    environment("TEST_PATH_NORMAL_ZIP_PATH", "$testDir/test_path_normal.zip")
+    environment("TEST_BZ2_PATH", "$testDir/test.bz2")
+    environment("TEST_TARBZ2_PATH", "$testDir/test.tar.bz2")
+}
+
+tasks.withType(Test::class.java).configureEach {
+    dependsOn(generateTestFiles)
+    failOnNoDiscoveredTests.set(false)
+    val testDir = layout.buildDirectory.dir("tmp/large_tests").get().asFile.absolutePath
+    systemProperty("TEST_ZIP_PATH", "$testDir/test.zip")
+    systemProperty("TEST_7Z_PATH", "$testDir/test.7z")
+    systemProperty("TEST_TARGZ_PATH", "$testDir/test.tar.gz")
+    systemProperty("TEST_BZ2_PATH", "$testDir/test.bz2")
+    systemProperty("TEST_TARBZ2_PATH", "$testDir/test.tar.bz2")
+    systemProperty("TEST_SJIS_ZIP_PATH", "$testDir/test_sjis.zip")
+    systemProperty("TEST_PATH_NORMAL_ZIP_PATH", "$testDir/test_path_normal.zip")
+    systemProperty("TEST_BULK_ZIP_PATH", "$testDir/test_bulk.zip")
+    systemProperty("TEST_EXT_ZIP_PATH", "$testDir/test_ext.zip")
+    systemProperty("LARGE_ZIP_PATH", "$testDir/large.zip")
+    systemProperty("LARGE_7Z_PATH", "$testDir/large.7z")
+    systemProperty("LARGE_TARGZ_PATH", "$testDir/large.tar.gz")
+    systemProperty("LARGE_100M_7Z_PATH", "$testDir/large_100m.7z")
+    systemProperty("LARGE_100M_TARGZ_PATH", "$testDir/large_100m.tar.gz")
+    systemProperty("LARGE_100M_ZIP_PATH", "$testDir/large_100m.zip")
+    systemProperty("LARGE_TARBZ2_PATH", "$testDir/large.tar.bz2")
+    systemProperty("LARGE_100M_TARBZ2_PATH", "$testDir/large_100m.tar.bz2")
+}
+
+tasks.configureEach {
+    if (name == "jsBrowserTest" || name == "jsNodeTest") {
+        dependsOn(copyJsTestAssets)
+    }
+    if (name == "wasmJsBrowserTest" || name == "wasmJsNodeTest") {
+        dependsOn(copyWasmTestAssets)
+    }
+}
--- kioarch/build.gradle.kts
+++ kioarch/build.gradle.kts
@@ -1,5 +1,4 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
-import org.gradle.api.tasks.testing.Test
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
@@ -10,6 +9,7 @@ plugins {
     id("kioarch.versioning")
     id("kioarch.detekt")
     id("kioarch.natives")
+    id("kioarch.testing")
 }
 
 kotlin {
@@ -186,95 +186,5 @@ mavenPublishing {
     }
 }
 
-val generateTestFiles by tasks.registering(JavaExec::class) {
-    group = "verification"
-    description = "Generates test archives for testing"
-    dependsOn("jvmTestClasses")
-    classpath = kotlin.targets.getByName("jvm").compilations.getByName("test").let {
-        files(it.output.allOutputs, it.runtimeDependencyFiles)
-    }
-    mainClass.set("com.sorrowblue.kioarch.TestFileGenerator")
-    args(layout.buildDirectory.dir("tmp/large_tests").get().asFile.absolutePath)
-}
 
-tasks.withType<org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeTest>().configureEach {
-    dependsOn(generateTestFiles)
-    val testDir = layout.buildDirectory.dir("tmp/large_tests").get().asFile.absolutePath
-    environment("LARGE_ZIP_PATH", "$testDir/large.zip")
-    environment("LARGE_7Z_PATH", "$testDir/large.7z")
-    environment("LARGE_TARGZ_PATH", "$testDir/large.tar.gz")
-    environment("LARGE_100M_7Z_PATH", "$testDir/large_100m.7z")
-    environment("LARGE_100M_TARGZ_PATH", "$testDir/large_100m.tar.gz")
-    environment("LARGE_100M_ZIP_PATH", "$testDir/large_100m.zip")
-    environment("TEST_BZ2_PATH", "$testDir/test.bz2")
-    environment("TEST_TARBZ2_PATH", "$testDir/test.tar.bz2")
-
-    // Pass environment variables to the iOS Simulator test runner
-    environment("SIMCTL_CHILD_LARGE_ZIP_PATH", "$testDir/large.zip")
-    environment("SIMCTL_CHILD_LARGE_7Z_PATH", "$testDir/large.7z")
-    environment("SIMCTL_CHILD_LARGE_TARGZ_PATH", "$testDir/large.tar.gz")
-    environment("SIMCTL_CHILD_LARGE_100M_7Z_PATH", "$testDir/large_100m.7z")
-    environment("SIMCTL_CHILD_LARGE_100M_TARGZ_PATH", "$testDir/large_100m.tar.gz")
-    environment("SIMCTL_CHILD_LARGE_100M_ZIP_PATH", "$testDir/large_100m.zip")
-    environment("SIMCTL_CHILD_TEST_BZ2_PATH", "$testDir/test.bz2")
-    environment("SIMCTL_CHILD_TEST_TARBZ2_PATH", "$testDir/test.tar.bz2")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest>().configureEach {
-    dependsOn(generateTestFiles)
-    val testDir = layout.buildDirectory.dir("tmp/large_tests").get().asFile.absolutePath
-    environment("LARGE_ZIP_PATH", "$testDir/large.zip")
-    environment("LARGE_7Z_PATH", "$testDir/large.7z")
-    environment("LARGE_TARGZ_PATH", "$testDir/large.tar.gz")
-    environment("TEST_7Z_PATH", "$testDir/test.7z")
-    environment("TEST_ZIP_PATH", "$testDir/test.zip")
-    environment("TEST_SJIS_ZIP_PATH", "$testDir/test_sjis.zip")
-    environment("TEST_PATH_NORMAL_ZIP_PATH", "$testDir/test_path_normal.zip")
-    environment("TEST_BZ2_PATH", "$testDir/test.bz2")
-    environment("TEST_TARBZ2_PATH", "$testDir/test.tar.bz2")
-}
-
-tasks.withType<Test>().configureEach {
-    dependsOn(generateTestFiles)
-    failOnNoDiscoveredTests.set(false)
-    val testDir = layout.buildDirectory.dir("tmp/large_tests").get().asFile.absolutePath
-    systemProperty("TEST_ZIP_PATH", "$testDir/test.zip")
-    systemProperty("TEST_7Z_PATH", "$testDir/test.7z")
-    systemProperty("TEST_TARGZ_PATH", "$testDir/test.tar.gz")
-    systemProperty("TEST_BZ2_PATH", "$testDir/test.bz2")
-    systemProperty("TEST_TARBZ2_PATH", "$testDir/test.tar.bz2")
-    systemProperty("TEST_SJIS_ZIP_PATH", "$testDir/test_sjis.zip")
-    systemProperty("TEST_PATH_NORMAL_ZIP_PATH", "$testDir/test_path_normal.zip")
-    systemProperty("TEST_BULK_ZIP_PATH", "$testDir/test_bulk.zip")
-    systemProperty("TEST_EXT_ZIP_PATH", "$testDir/test_ext.zip")
-    systemProperty("LARGE_ZIP_PATH", "$testDir/large.zip")
-    systemProperty("LARGE_7Z_PATH", "$testDir/large.7z")
-    systemProperty("LARGE_TARGZ_PATH", "$testDir/large.tar.gz")
-    systemProperty("LARGE_100M_7Z_PATH", "$testDir/large_100m.7z")
-    systemProperty("LARGE_100M_TARGZ_PATH", "$testDir/large_100m.tar.gz")
-    systemProperty("LARGE_100M_ZIP_PATH", "$testDir/large_100m.zip")
-    systemProperty("LARGE_TARBZ2_PATH", "$testDir/large.tar.bz2")
-    systemProperty("LARGE_100M_TARBZ2_PATH", "$testDir/large_100m.tar.bz2")
-}
-
-val copyJsTestAssets by tasks.registering(Copy::class) {
-    dependsOn(generateTestFiles)
-    from(layout.buildDirectory.dir("tmp/large_tests"))
-    into(project.rootProject.layout.buildDirectory.dir("js/packages/KioArch-root-kioarch-test/kotlin"))
-}
-
-val copyWasmTestAssets by tasks.registering(Copy::class) {
-    dependsOn(generateTestFiles)
-    from(layout.buildDirectory.dir("tmp/large_tests"))
-    into(project.rootProject.layout.buildDirectory.dir("wasm/packages/KioArch-root-kioarch-test/kotlin"))
-}
-
-tasks.configureEach {
-    if (name == "jsBrowserTest" || name == "jsNodeTest") {
-        dependsOn(copyJsTestAssets)
-    }
-    if (name == "wasmJsBrowserTest" || name == "wasmJsNodeTest") {
-        dependsOn(copyWasmTestAssets)
-    }
-}
 
